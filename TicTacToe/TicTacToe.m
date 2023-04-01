54ea5214a6bc2bdf71cbbf33c8c1b2ff003e9365
Jaylon Nelson-Sellers <jcns@uw.edu>
2023-03-31 18:32:35 -0700

    bots can now play each other

--- TicTacToe/TicTacToe.m
+++ TicTacToe/TicTacToe.m
@@ -14,6 +14,7 @@ classdef TicTacToe
         function obj = playHumanBotGame(obj, Bot)
             % Loop until the game is over
             while true
+                disp( obj.board);
 
                 % Check if the move is valid
                 while true
@@ -70,6 +71,47 @@ classdef TicTacToe
             end
 
         end
+
+        function obj = playBotBotGame(obj, Bot, Bot2)
+            % Loop until the game is over
+            while true
+
+                fprintf('The bot is making a move...\n');
+                [row, col] = Bot.bot_move(obj.board);
+                obj.board(row, col) = 1;
+                disp( obj.board);
+                % Check if the bot has won
+                if check_win(obj.board, 1)
+                    fprintf('Bot 1 wins!\n');
+                    break;
+                end
+
+                % Check if the game is a tie
+                if check_tie(obj.board)
+                    fprintf('The game is a tie.\n');
+                    break;
+                end
+
+                % Get the bot's move
+                fprintf('The bot is making a move...\n');
+                [row, col] = Bot.bot_move(obj.board);
+                obj.board(row, col) = 2;
+                disp( obj.board);
+                % Check if the bot has won
+                if check_win(obj.board, 2)
+                    fprintf('Bot 2 wins!\n');
+                    break;
+                end
+
+                % Check if the game is a tie
+                if check_tie(obj.board)
+                    fprintf('The game is a tie.\n');
+                    break;
+                end
+            end
+
+        end
+
     end
 end
 
--- TicTacToe/TicTacToeBot.m
+++ TicTacToe/TicTacToeBot.m
@@ -4,14 +4,18 @@ classdef TicTacToeBot
     
     properties
         Brain
+        Score
     end
     
     methods
         function obj = TicTacToeBot()
             obj.Brain = PerceptronLayer(100, 10,"hardlim");
+            obj.Score = Glicko2Score(1500, 350, 0.06, .75)
         end
 
         function [row, col] = bot_move(obj, board)
+            pause(2);
+
             % Choose a random empty spot
             [row, col] = find(board == 0);
             idx = randi(length(row));
